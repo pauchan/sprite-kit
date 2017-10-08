c58443b54e8a1d200abfe1e9582801ced764474c
Pawel Rusin <pawel-rusin@cookpad.com>
2017-10-08 17:37:27 +0900

    knight fights

--- SpriteKitTest/GameScene.swift
+++ SpriteKitTest/GameScene.swift
@@ -12,10 +12,16 @@ import GameplayKit
 class GameScene: SKScene {
 
     let player = SKSpriteNode(imageNamed: "Warrior_idle")
+    let background = SKSpriteNode(imageNamed: "background")
+
 
     override func didMove(to view: SKView) {
-        backgroundColor = SKColor.white
-        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+
+        background.position = CGPoint(x: frame.size.width/2.0, y: frame.size.height/2.0)
+        background.size = frame.size
+        background.zPosition = -1.0
+        addChild(background)
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.4)
         addChild(player)
         run(SKAction.repeatForever(SKAction.sequence([
             SKAction.run(addMonster),
@@ -33,20 +39,20 @@ class GameScene: SKScene {
 
     func addMonster() {
         let monster = SKSpriteNode(imageNamed: "Orc_walking")
-        let actualY = random(min: monster.size.height / 2, max: size.height - monster.size.height / 2)
-        monster.position = CGPoint(x: size.width + monster.size.width / 2, y: actualY)
+        monster.position = CGPoint(x: size.width + monster.size.width / 2, y: size.height * 0.4)
         addChild(monster)
         let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
-        let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: actualY) , duration: TimeInterval(actualDuration))
+        let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: size.height * 0.4) , duration: TimeInterval(actualDuration))
         let actionMoveDone = SKAction.removeFromParent()
         monster.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        //guard let touch = touches.first else { return }
-        //let touchLocation = touch.location(in: self)
-        player.run()
-
+        let fightAtlas = SKTextureAtlas(named: "Warrior_fighting")
+        let textures = fightAtlas.textureNames.sorted().map {
+            fightAtlas.textureNamed($0)
+        }
+        player.run(SKAction.animate(with: textures, timePerFrame: 0.1))
     }
 
 }
